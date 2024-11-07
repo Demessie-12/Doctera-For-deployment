310d7b67e0b0437ce0841d503d453f46760f27ec
demessietry <demessiewondimu12@gmail.com>
2024-11-07 21:19:40 +0300

    latest

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,14 +3,21 @@ const {
   signupUser,
   loginUser,
   logoutUser,
+  updateMyProfile,
+  updateMyPassword,
   loginAdmin,
 } = require("../controllers/auth.controllers.js");
+
+const { checkLogin } = require("../middleware/authController.js");
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/loginAdmin", loginAdmin);
+router.patch("/profile/:username", checkLogin, updateMyProfile);
+router.patch("/password/:username", checkLogin, updateMyPassword);
+
 router.get("/logout", logoutUser);
 
 module.exports = router;
